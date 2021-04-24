266a8ab872bdf9da54e15018f4db6187f859161e
Pritham Immanuel Ilango <prithamimmanuel@gmail.com>
2021-04-24 15:36:21 +0530

    sql_test delet

--- new_project/backend/sql_test.js
+++
@@ -1,9 +0,0 @@
-const mysql=require("mysql2")
-const pool=mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'my_db',
-    password: 'password'
-});
-
-module.exports=pool.promise();
